658ece1a992bf113d40d5f5bb979c31da1ef2272
challappamanasa2005-design <challappamanasa2005@gmail.com>
2025-12-19 10:06:44 +0530

    Add files via upload

---
+++ Examplefordisadvantageofmultithreading.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Printer implements Runnable{
+    synchronized public void run(){
+    String name=Thread.currentThread().getName();
+    System.out.println(name+" printing started");
+    for(int i=1;i<=5;i++){
+        System.out.println(name +"is printing");
+        try{
+            Thread.sleep(2000);
+        }
+        catch(Exception e){
+            System.out.println("some problem occured");
+        }
+    }
+    System.out.println(name+"completed printing");
+}
+}
+public class Examplefordisadvantageofmultithreading {
+    public static void main(String[] args) {
+        Printer p=new Printer();
+        Thread t1=new Thread(p);
+        Thread t2=new Thread(p);
+        Thread t3=new Thread(p);
+        t1.setName("manasa");
+        t2.setName("likhitha");
+        t3.setName("akhila");
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
