7b8a7c30f7a0b51dab9037a180511a9e736bf6ea
challappamanasa2005-design <challappamanasa2005@gmail.com>
2025-12-23 17:20:01 +0530

    Add files via upload

---
+++ Exampleforhashtable.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Exampleforhashtable {
+    public static void main(String[] args) {
+        Hashtable<String,Object> ht=new Hashtable<String,Object>();
+        ht.put("Name","manasa");
+        ht.put("age",20);
+        ht.put("email","challappa@2710");
+        System.out.println(ht);
+
+    }
+}
---
+++ Exampleforstring.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Exampleforstring {
+    public static void main(String[] args) {
+     /* String s= new String("manasa"); */
+     /*  System.out.println(s); */
+     /*  s.concat("alluarjun"); */
+     /*  System.out.println(s); */
+    /*  */
+    StringBuffer sb=new StringBuffer("manasa");
+    System.out.println(sb);
+    sb.append("alluarjun");
+    System.out.println(sb);
+    }
+}
